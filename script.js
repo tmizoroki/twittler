f8468d3eb2534c3bd34e21bb0bd1bb469d2adf5f
Tomio Mizoroki <tmizoroki@gmail.com>
2015-07-06 23:06:00 -0700

    Modify script to display tweets in display-twits section

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-  var $body = $('body');
-  $body.html('');
+  var $twits = $('.display-twits');
+  $twits.html('');
 
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
     var $tweet = $('<div></div>');
     $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
+    $tweet.appendTo($twits);
     index -= 1;
   }
 
