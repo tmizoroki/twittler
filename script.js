395d17fcf4f8c6fab3bdf6d09db8d84a9ea9d531
Tomio Mizoroki <tmizoroki@gmail.com>
2015-07-08 18:50:49 -0700

    Add ability to return to home page. Fix user-info section display issues.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+  //$('.logo').attr('data-username', '');
+
   var numFollowing = Object.keys(streams.users).length.toString();
   $('.following-count').text(numFollowing);
 
@@ -8,7 +10,12 @@ $(document).ready(function(){
 
   var lastLength = 0;
 
+  /*var updateStats = function() {
+    $('.twit-count').text(source.length);
+  };*/
+
   var findNewTweets = function(username) {
+    //console.log(username);
     var source = username ? streams.users[username] : streams.home;
     if (source.length > lastLength) {
       numNewTwits = source.length - lastLength;
@@ -24,7 +31,7 @@ $(document).ready(function(){
       console.log(newTweets);
       newTweets.forEach(function(tweet) {
         var $tweet = $('<div></div>').addClass('twit-body group').attr('data-username', tweet.user);
-        var $profilepic = $('<img>').attr('src', 'assets/images/' + tweet.user + '.jpg');
+        var $profilepic = $('<img>').addClass('message-pic').attr('src', 'assets/images/' + tweet.user + '.jpg');
         var $user = $('<div></div>').addClass('username').text('@' + tweet.user);
         var $time = $('<div></div>').addClass('date').text(tweet.created_at);
         var $message = $('<div></div>').addClass('user-message').text(tweet.message);
@@ -39,16 +46,21 @@ $(document).ready(function(){
 
   var intervalId = setInterval(addNewTweets, 1000);
 
-  $('.display-twits').on('click', '.username' || 'img', function() {
+  $('body').on('click', '.username, .message-pic, .logo', function() {
     $('.twit-body').hide();
     lastLength = 0;
     clearInterval(intervalId);
-    $username = $(this).closest('.twit-body').data('username');
-    setInterval(addNewTweets, 1000, $username);
-
-    $('.visitor h2').text('@' + $username);
-    $('.visitor img').attr('src', 'assets/images/' + $username + '.jpg');
-    $('.twit-count').text(streams.users[$username].length);
+    $username = $('[data-username]') ? $(this).closest('[data-username]').data('username') : undefined;
+    console.log($username);
+    intervalId = setInterval(addNewTweets, 1000, $username);
+    if ($username === null) {
+      $('.visitor h2').text('@visitor');
+      $('.visitor img').attr('src', 'assets/images/user.png');
+    } else {
+      $('.visitor h2').text('@' + $username);
+      $('.visitor img').attr('src', 'assets/images/' + $username + '.jpg');
+      $('.twit-count').text(streams.users[$username].length);
+    }
   });
 
 });
